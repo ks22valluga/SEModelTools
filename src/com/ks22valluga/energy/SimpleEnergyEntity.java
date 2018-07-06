540d4a307305a4abab6990c31e6d138f33edad92
kevins <kevin.stewart@passivsystems.com>
2018-07-06 19:06:59 +0100

    'first'

---
+++ src/com/ks22valluga/energy/SimpleEnergyEntity.java
@@ -0,0 +1,11 @@
+package com.ks22valluga.energy;
+
+import java.math.BigDecimal;
+
+public class SimpleEnergyEntity {
+private BigDecimal energyMeter;
+private BigDecimal maxPower;
+private String friendlyName;
+
+	
+}
