0659dcec4bb635260e4e78bdcd330463d8392269
kevins <kevin.stewart@passivsystems.com>
2018-07-15 14:58:36 +0100

    Interm

--- src/com/ks22valluga/energy/SimpleEnergyEntity.java
+++ src/com/ks22valluga/energy/SimpleEnergyEntity.java
@@ -1,12 +1,18 @@
 package com.ks22valluga.energy;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public abstract class SimpleEnergyEntity {
 private BigDecimal energyMeterA,energyMeterB;
 private BigDecimal maxPowerGen,maxPowerConsume,maxEnergyStorage;
 private String friendlyNodeName,friendlyNameEMA,friendlyNameEMB;
 private NodeTypeEnum nte;
+private ArrayList<SimpleEnergyEntity> directChildren;
+private ArrayList<SimpleEnergyEntity> meters;
+private ArrayList<SimpleEnergyEntity> meterUpstreamNodes;
+private ArrayList<SimpleEnergyEntity> meterDownStreamNodes;
+private SimpleEnergyEntity parentNode;
 
 public SimpleEnergyEntity(BigDecimal energyMeterA, BigDecimal energyMeterB,
 		BigDecimal maxPowerGen, BigDecimal maxPowerConsume,
@@ -21,12 +27,28 @@ public SimpleEnergyEntity(BigDecimal energyMeterA, BigDecimal energyMeterB,
 	this.friendlyNameEMA = friendlyNameEMA;
 	this.friendlyNameEMB = friendlyNameEMB;
 	this.nte = nte;
+	this.directChildren = new ArrayList<>();
+	this.parentNode=null;
+	this.meters=new ArrayList<>();
+	this.meterDownStreamNodes = new ArrayList<>();
+	this.meterUpstreamNodes = new  ArrayList<>();
 }
+
+
+public void attachToParent(SimpleEnergyEntity see){
+	this.parentNode=see;
+	see.addChildNode(this);
+	
+}
+private void addChildNode(SimpleEnergyEntity see) {
+	this.directChildren.add(see);
+}
+
 
 //TODO: create methods that model the behaviour each node type
 
 public abstract BigDecimal getCurrentPowerConsumption();
-public abstract BigDecimal getCurrentPowerGeneration();
+//NB generation is negative consumption
 
 	
 }
